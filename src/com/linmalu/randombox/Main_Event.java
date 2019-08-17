46648a7858d4d08d7d06a1000f03e6d9911c8359
Linmalu <gkdwns0103@naver.com>
2019-08-18 01:28:03 +0900

    1.13.2.0 이름 변경

--- src/com/linmalu/randombox/Main_Event.java
+++ src/com/linmalu/randombox/Main_Event.java
@@ -1,68 +1,57 @@
 package com.linmalu.randombox;
 
+import com.linmalu.library.api.LinmaluEvent;
+import com.linmalu.library.api.LinmaluMain;
 import com.linmalu.library.api.LinmaluServer;
-import com.linmalu.library.api.LinmaluTellraw;
-import com.linmalu.randombox.data.LinmaluInventory;
-import org.bukkit.ChatColor;
 import org.bukkit.Effect;
 import org.bukkit.Material;
-import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.ItemStack;
 
-public class Main_Event implements Listener
+public class Main_Event extends LinmaluEvent
 {
-	private LinmaluInventory inventory = Main.getMain().getLinmaluInventory();
+	private final RandomBoxManager _randomBoxManager = Main.getInstance().getRandomBoxManager();
+
+	public Main_Event(LinmaluMain main)
+	{
+		super(main);
+	}
 
 	@EventHandler
-	public void Event(PlayerJoinEvent event)
+	public void event(PlayerJoinEvent event)
 	{
 		Player player = event.getPlayer();
 		if(player.isOp())
 		{
-			LinmaluServer.version(Main.getMain(), player);
+			LinmaluServer.version(Main.getInstance(), player);
 		}
 	}
 
 	@EventHandler
-	public void Event(PlayerInteractEvent event)
+	public void event(PlayerInteractEvent event)
 	{
 		if(event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.TRAPPED_CHEST)
 		{
 			Player player = event.getPlayer();
-			if(inventory.hasItem())
+			if(_randomBoxManager.useRandomBox(player))
 			{
 				event.getClickedBlock().setType(Material.AIR);
-				ItemStack item = inventory.getItem();
-				player.getInventory().addItem(item).values().forEach(Item ->
-				{
-					player.getWorld().dropItem(player.getLocation(), Item);
-				});
-				LinmaluTellraw.sendItem(player, item, Main.getMain().getTitle() + ChatColor.GREEN + (item.getItemMeta().hasDisplayName() ? item.getItemMeta().getDisplayName() : item.getType().toString()) + ChatColor.GOLD + "(" + item.getAmount() + ")" + ChatColor.GRAY + " 아이템을 획득했습니다.");
-				player.getWorld().playEffect(event.getClickedBlock().getLocation(), Effect.STEP_SOUND, Material.TRAPPED_CHEST);
-				player.getWorld().playSound(event.getClickedBlock().getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
-			}
-			else
-			{
-				player.sendMessage(Main.getMain().getTitle() + ChatColor.YELLOW + "아이템이 없습니다.");
-				player.getWorld().playSound(event.getClickedBlock().getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1, 1);
+				event.getClickedBlock().getWorld().playEffect(event.getClickedBlock().getLocation(), Effect.STEP_SOUND, Material.TRAPPED_CHEST);
 			}
 			event.setCancelled(true);
 		}
 	}
 
 	@EventHandler
-	public void Event(InventoryCloseEvent event)
+	public void event(InventoryCloseEvent event)
 	{
-		if(event.getInventory().getTitle().startsWith(Main.getMain().getTitle()))
+		if(event.getInventory().getHolder() instanceof RandomBoxHolder)
 		{
-			inventory.save(event.getInventory());
+			_randomBoxManager.save(((RandomBoxHolder)event.getInventory().getHolder()).getTitle());
 		}
 	}
 }
