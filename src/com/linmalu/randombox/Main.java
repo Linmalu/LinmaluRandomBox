902e3e2280cf7806bd35878f5624818ced164cb5
Linmalu <gkdwns0103@naver.com>
2019-08-17 23:47:24 +0900

    1.13.2.0 이름변경 및 로딩 로그 추가

--- src/com/linmalu/randombox/Main.java
+++ src/com/linmalu/randombox/Main.java
@@ -1,27 +1,28 @@
 package com.linmalu.randombox;
 
 import com.linmalu.library.api.LinmaluMain;
-import com.linmalu.randombox.data.LinmaluInventory;
 
 public class Main extends LinmaluMain
 {
-	public static Main getMain()
+	public static Main getInstance()
 	{
 		return (Main)LinmaluMain.getInstance();
 	}
 
-	private LinmaluInventory inventory;
+	private RandomboxManager _randomboxManager;
 
 	public void onEnable()
 	{
 		super.onEnable();
-		inventory = new LinmaluInventory();
+		getLogger().info("Loading . . . . .");
+		_randomboxManager = new RandomboxManager();
+		getLogger().info("Loading Complete!");
 		registerCommand(new Main_Command());
 		registerEvents(new Main_Event());
 	}
 
-	public LinmaluInventory getLinmaluInventory()
+	public RandomboxManager getRandomboxManager()
 	{
-		return inventory;
+		return _randomboxManager;
 	}
 }
